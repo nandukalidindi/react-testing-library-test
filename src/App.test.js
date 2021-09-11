a7b5c5941ede1523fc8763ce2a318c95eee1b7cd
Nandu Kalidindi <nandukalidindi@gmail.com>
2021-09-11 18:01:25 -0400

    Working two simple test cases for TODO list

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByText, getByLabelText } = render(<App />);
+
+  getByText("TODOS");
+  getByLabelText("What needs to be done?");
+  getByText("Add #1");
+});
+
+test("allows users to add items to their list", () => {
+  const { getByText, getByLabelText } = render(<App />);
+
+  const input = getByLabelText("What needs to be done?");
+  const button = getByText("Add #1");
+
+  // Simulate user events
+  fireEvent.change(input, { target: { value: "Learn spanish" } });
+  fireEvent.click(button);
+
+  // Make assertion
+  getByText("Learn spanish");
+  getByText("Add #2");
 });
